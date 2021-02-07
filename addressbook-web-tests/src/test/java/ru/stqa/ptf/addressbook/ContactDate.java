889f993163252205cfb5bab66bd8cce6b5f93c86
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-08 01:23:18 +0300

    Создан класс ContactDate

---
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/ContactDate.java
@@ -0,0 +1,38 @@
+package ru.stqa.ptf.addressbook;
+
+public class ContactDate {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String company;
+  private final String home;
+  private final String email;
+
+  public ContactDate(String firstName, String middleName, String lastName, String company, String home, String email) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.company = company;
+    this.home = home;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getCompany() { return company; }
+
+  public String getHome() { return home; }
+
+  public String getEmail() { return email; }
+}
+
