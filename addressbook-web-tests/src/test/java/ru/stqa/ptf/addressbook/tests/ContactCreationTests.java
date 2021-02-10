55cbbe2a9ae98aabae7eab9b374c93b04dca8b6f
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-11 02:54:57 +0300

    Рефакторинг кода в тесте создания контакта ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
@@ -1,79 +1,22 @@
 package ru.stqa.ptf.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
-
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactDate;
 
-public class ContactCreationTests {
-  private WebDriver wd;
+public class ContactCreationTests extends TestBase {
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
   @Test
-  public void testContactCreation() throws Exception {
-    initContactCreation();
-    fillContactForm(new ContactDate(
+  public void testContactCreation() {
+
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactDate(
             "Eva",
             "Victorovna",
             "Orlova",
             "OOO Test",
             "84832121212",
             "e.orlova@bk.ru"));
-    submitContactCreation();
-    returnToHomePage();
-  }
-  private void initContactCreation() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  private void fillContactForm(ContactDate contactDate) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).sendKeys(contactDate.getFirstName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).sendKeys(contactDate.getMiddleName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).sendKeys(contactDate.getLastName());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).sendKeys(contactDate.getCompany());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).sendKeys(contactDate.getHome());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).sendKeys(contactDate.getEmail());
-  }
-
-  private void submitContactCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  private void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.findElement(By.linkText("Logout")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("");
-    wd.quit();
+    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().returnToHomePage();
   }
 }
