653e67a56f80579689a50ddcb9cb6de911ad9080
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-10 23:06:24 +0300

    Рефакторинг кода в NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/NavigationHelper.java
@@ -3,24 +3,13 @@ package ru.stqa.ptf.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class NavigationHelper {
-  private ChromeDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(ChromeDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
-  public void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    click(By.linkText("groups"));
   }
 }
