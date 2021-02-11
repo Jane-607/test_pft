05c05a4568f2bbf10b52f9d9c410027883e71ec1
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-11 03:03:42 +0300

    Добавлен новый класс- помощник ContactHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,40 @@
+package ru.stqa.ptf.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.stqa.ptf.addressbook.model.ContactDate;
+
+public class ContactHelper extends HelperBase {
+  public ContactHelper(ChromeDriver wd) {
+    super(wd);
+  }
+
+  public void returnToHomePage() {
+    click(By.linkText("home"));
+  }
+
+  public void submitContactCreation() {
+    click(By.name("submit"));
+  }
+
+  public void fillContactForm(ContactDate contactDate) {
+    type(By.name("firstname"), contactDate.getFirstName());
+    type(By.name("middlename"), contactDate.getMiddleName());
+    type(By.name("lastname"), contactDate.getLastName());
+    type(By.name("company"), contactDate.getCompany());
+    type(By.name("home"), contactDate.getHome());
+    type(By.name("email"), contactDate.getEmail());
+  }
+
+  public void initContactCreation() {
+    click(By.linkText("add new"));
+  }
+
+  public void deleteSelectedContacts() {
+    click(By.xpath("//input[@value='Delete']"));
+  }
+
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
+}
