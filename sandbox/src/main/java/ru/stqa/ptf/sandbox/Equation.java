3841258adf5a76aee7ba6079b290470ae88c27b8
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-13 01:58:11 +0300

    Sandbox. Main. Добавлены дополнительные условия в класс Equation

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Equation.java
@@ -17,14 +17,24 @@ public class Equation {
 
     if (a == 0) {
       if (b == 0) {
-        if (c == 0) { n = -1; }
-    else { n = 0; } }
-        else { n = 1; }
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
       }
-
-    if (d > 0) { n = 2; }
-    else { if (d == 0) { n = 1; }
-    else { n = 0; }
+    } else {
+      if (d > 0) {
+        n = 2;
+      } else {
+        if (d == 0) {
+          n = 1;
+        } else {
+          n = 0;
+        }
+      }
     }
   }
     public int rootNunber () {
