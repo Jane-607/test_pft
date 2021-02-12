4bb358bcaec51b41a447a19072f9b7dd0653c8a7
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-12 23:21:33 +0300

    Создан тест модификации первого контакта из списка

---
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,22 @@
+package ru.stqa.ptf.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.ContactDate;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void ContactModificationTests() {
+    app.getContactHelper().returnToHomePage();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactDate(
+            "Eva",
+            "Victorovna",
+            "Orlova",
+            "OOO Test",
+            "84832121212",
+            "e.orlova@bk.ru"));
+    app.getContactHelper().submitContacModification();
+    app.getContactHelper().returnToHomePage();
+    }
+}
