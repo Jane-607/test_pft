7991f8dfe1b7100a6aa18bdb15abf0564b3ca767
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-12 22:48:19 +0300

    Создан тест модификации первой группы из списка

---
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.ptf.addressbook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.GroupDate;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void GroupModificationTests() {
+
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupDate("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+    }
+
+}
