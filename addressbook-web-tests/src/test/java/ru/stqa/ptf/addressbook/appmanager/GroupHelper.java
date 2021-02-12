43ab5077896e32248ceffb027aae338f68189f13
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-12 22:46:36 +0300

    Добавлены новые методы в GroupHelper

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/GroupHelper.java
@@ -10,9 +10,7 @@ public class GroupHelper extends HelperBase {
     super(wd);
   }
 
-  public void returnToGroupPage() {
-    click(By.linkText("group page"));
-  }
+  public void returnToGroupPage() { click(By.linkText("group page")); }
 
   public void submitGroupCreation() {
     click(By.name("submit"));
@@ -35,4 +33,13 @@ public class GroupHelper extends HelperBase {
   public void selectGroup() {
     click(By.name("selected[]"));
   }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
+
 }
