73dd596f61f4857affe211e23be4195522b13ec4
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-18 01:15:34 +0300

    Addressbook. Test. Класс GroupDeletionTests. Проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.ptf.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.GroupDate;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
