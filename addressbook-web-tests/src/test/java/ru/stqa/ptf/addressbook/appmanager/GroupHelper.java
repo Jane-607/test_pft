6083a0a1bf1a16376779f19a12b22ac3fa507878
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-18 01:14:02 +0300

    Addressbook. Test. Класс GroupHelper. Добавлены методы создания группы и выбора чекбокса

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/GroupHelper.java
@@ -42,4 +42,14 @@ public class GroupHelper extends HelperBase {
     click(By.name("update"));
   }
 
+  public void createGroup(GroupDate group) {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  public boolean isThereAGroup() {
+    return isElementPresent(By.name("selected[]"));
+  }
 }
