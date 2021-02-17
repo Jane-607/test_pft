e2141ac9232a7507141bd8218126f7090a9672d0
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-18 01:22:08 +0300

    Addressbook. Test. Класс GroupModificationTests. Проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
@@ -9,6 +9,9 @@ public class GroupModificationTests extends TestBase {
   public void GroupModificationTests() {
 
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupDate("test1", "test2", "test3"));
