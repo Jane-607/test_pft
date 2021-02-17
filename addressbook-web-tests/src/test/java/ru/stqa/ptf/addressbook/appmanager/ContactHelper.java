f4c5d5389f195b6acc6163da962cd3638d274623
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-17 23:08:11 +0300

    Addressbook. Test. Для класса HelperBase в метод fillContactForm добавлен параметр и условие к нему

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
@@ -1,7 +1,10 @@
 package ru.stqa.ptf.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import ru.stqa.ptf.addressbook.model.ContactDate;
 
 public class ContactHelper extends HelperBase {
@@ -16,13 +19,19 @@ public class ContactHelper extends HelperBase {
 
   public void submitContactCreation() { click(By.name("submit")); }
 
-  public void fillContactForm(ContactDate contactDate) {
+  public void fillContactForm(ContactDate contactDate, boolean creation) {
     type(By.name("firstname"), contactDate.getFirstName());
     type(By.name("middlename"), contactDate.getMiddleName());
     type(By.name("lastname"), contactDate.getLastName());
     type(By.name("company"), contactDate.getCompany());
     type(By.name("home"), contactDate.getHome());
     type(By.name("email"), contactDate.getEmail());
+
+    if (creation) {
+      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactDate.getGroup());
+    } else {
+      Assert.assertFalse(isElementPresent(By.name("new_group")));
+    }
   }
 
   public void initContactCreation() {
