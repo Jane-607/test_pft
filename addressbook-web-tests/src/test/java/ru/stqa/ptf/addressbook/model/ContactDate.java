fd7962e1bf791e12c174d77c9f95b1a472cc7aea
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-17 22:55:29 +0300

    Addressbook. Test. В класс ContactDate добавлено поле группы

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/ContactDate.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/ContactDate.java
@@ -7,14 +7,16 @@ public class ContactDate {
   private final String company;
   private final String home;
   private final String email;
+  private final String group;
 
-  public ContactDate(String firstName, String middleName, String lastName, String company, String home, String email) {
+  public ContactDate(String firstName, String middleName, String lastName, String company, String home, String email, String  group) {
     this.firstName = firstName;
     this.middleName = middleName;
     this.lastName = lastName;
     this.company = company;
     this.home = home;
     this.email = email;
+    this.group = group;
   }
 
   public String getFirstName() {
@@ -34,5 +36,7 @@ public class ContactDate {
   public String getHome() { return home; }
 
   public String getEmail() { return email; }
+
+  public String getGroup() { return group; }
 }
 
