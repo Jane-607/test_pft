489148d738e5a2c639bee879def1940e990d3bfd
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-19 23:40:09 +0300

    Addressbook. Test. ContactModificationTests. Добавлена проверка наличия контакта и группы в списке

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactModificationTests.java
@@ -2,11 +2,29 @@ package ru.stqa.ptf.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactDate;
+import ru.stqa.ptf.addressbook.model.GroupDate;
 
 public class ContactModificationTests extends TestBase {
 
   @Test
   public void ContactModificationTests() {
+
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
+    }
+
+    app.getNavigationHelper().returnToHomePage();
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactDate(
+              "Eva",
+              "Victorovna",
+              "Orlova",
+              "OOO Test",
+              "84832121212",
+              "e.orlova@bk.ru",
+              "test1"), true);
+    }
     app.getNavigationHelper().returnToHomePage();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactDate(
