d8f69b0b3307ba193947b79ec999c83272290e84
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-19 22:57:25 +0300

    Addressbook. Test. testContactDeletion. Добавлены проверки наличия группы и контакта в списке

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactDeletionTests.java
@@ -1,11 +1,30 @@
 package ru.stqa.ptf.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.ContactDate;
+import ru.stqa.ptf.addressbook.model.GroupDate;
 
 public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() {
+
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
+    }
+
+    app.getNavigationHelper().returnToHomePage();
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactDate(
+              "Eva",
+              "Victorovna",
+              "Orlova",
+              "OOO Test",
+              "84832121212",
+              "e.orlova@bk.ru",
+              "test1"), true);
+    }
     app.getNavigationHelper().returnToHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
