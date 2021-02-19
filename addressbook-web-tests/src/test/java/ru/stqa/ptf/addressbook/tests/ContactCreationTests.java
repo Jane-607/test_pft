0a7a963162e106b8a586d49e65261f4ba964983e
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-19 22:34:11 +0300

    Addressbook. Test. ContactCreationTests. Добавлена проверка наличия группы в списке

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
@@ -1,15 +1,24 @@
 package ru.stqa.ptf.addressbook.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactDate;
+import ru.stqa.ptf.addressbook.model.GroupDate;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
 
-    app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactDate(
+    app.getGroupHelper().returnToGroupPage();
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
+    }
+    app.getNavigationHelper().returnToHomePage();
+    app.getContactHelper().createContact(new ContactDate(
             "Eva",
             "Victorovna",
             "Orlova",
@@ -17,7 +26,6 @@ public class ContactCreationTests extends TestBase {
             "84832121212",
             "e.orlova@bk.ru",
             "test1"), true);
-    app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().returnToHomePage();
   }
 }
