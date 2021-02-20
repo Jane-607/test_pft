d120b50e45cb317db3ae6885dade5540ce04ada8
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-02-20 21:38:46 +0300

    Sandbox.  Циклы и коллекции

---
+++ sandbox/src/test/java/ru/stqa/ptf/sandbox/PrimeTests.java
@@ -0,0 +1,28 @@
+package ru.stqa.ptf.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrimesFast() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test(enabled = false)
+  public void testPrimes() {
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+
+  @Test (enabled = false)
+  public void testNonPrimes() {
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE-2));
+  }
+
+  @Test(enabled = false)
+  public void testPrimesLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+}
