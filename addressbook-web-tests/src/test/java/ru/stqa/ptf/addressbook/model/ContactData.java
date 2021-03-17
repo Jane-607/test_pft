5b27bc9b4dfb3483746da6cc1423aa9fd224b11f
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-03-17 22:27:09 +0300

    Лекция 7.3. Условия отбора данных

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/ContactData.java
@@ -1,45 +1,89 @@
 package ru.stqa.ptf.addressbook.model;
-
 import com.google.gson.annotations.Expose;
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 
-@XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
-  @XStreamOmitField
+
+  @Id
+  @Column (name="id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column (name="firstname")
   private String firstName;
   @Expose
+  @Column (name="middlename")
   private String middleName;
+
   @Expose
+  @Column (name="lastname")
   private String lastName;
+
   @Expose
+  @Column (name="company")
   private String company;
+
   @Expose
+  @Column (name="address")
+  @Type(type = "text")
   private String address;
+
   @Expose
+  @Column (name="home")
+  @Type(type = "text")
   private String home;
+
   @Expose
+  @Column (name="mobile")
+  @Type(type = "text")
   private String mobile;
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            '}';
+  }
+
   @Expose
+  @Column (name="work")
+  @Type(type = "text")
   private String work;
+
   @Expose
+  @Column (name="email")
+  @Type(type = "text")
   private String email;
+
   @Expose
+  @Column (name="email2")
+  @Type(type = "text")
   private String email2;
+
   @Expose
+  @Column (name="email3")
+  @Type(type = "text")
   private String email3;
+
   @Expose
+  @Transient
   private String group;
 
+  @Transient
   private File photo;
 
+  @Transient
   private String allPhones;
+
+  @Transient
   private String allEmails;
 
 
@@ -81,15 +125,6 @@ public class ContactData {
 
   public String getAllPhones() { return allPhones; }
   public String getAllEmails() { return allEmails; }
-
-
-  @Override
-  public String toString() {
-    return "ContactData{" +
-            "id=" + id +
-            ", firstName='" + firstName + '\'' +
-            '}';
-  }
 
 
   @Override
