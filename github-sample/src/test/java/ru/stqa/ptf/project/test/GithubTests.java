f6db4b2cfa98bacde106c0315c3a9dd1debf848b
Jane-607 <78040798+Jane-607@users.noreply.github.com>
2021-04-07 02:01:01 +0300

    Лекция 9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/ptf/project/test/GithubTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.ptf.project.test;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.IdentityHashMap;
+
+public class GithubTests {
+
+  @Test
+
+  public void testCommits() throws IOException {
+
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("Jane-607", "test_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
